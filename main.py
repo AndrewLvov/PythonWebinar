00a6060b23647e18a1127008aaca93e0665578c6
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-09-27 11:58:37 +0300

    Happy toad and types

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import random
 
 
+def happy_toad():
+    h = "toad!"
+    # print("{}: {}".format(h, str(type(h))))
+    h = ['t', 'o', 'a', 'd']
+    # print("{}: {}".format(h, type(h)))
+    print("I'm a happy {}".format(h))
+    # print(type("I'm a happy {}".format(h)))
+    return 0
+
+
 def main():
-    for i in range(128):
+    for i in range(32):
+        # print("i: {}".format(type(i)))
         r = random.randrange(0, 1024)
+        # print("r: {}".format(type(r)))
+        print(r)
         if r % 5 == 0:
-            print(r, end=', ')
+            # print("r % 5 == 0: {}".format(type(r % 5 == 0)))
+            happy_toad()
 
 main()
