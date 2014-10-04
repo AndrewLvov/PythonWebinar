d1c332a2ab21138b7fc64881eeb52a8c2c904c7f
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-10-04 12:42:06 +0300

    Function parameters

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-import random
-
-from numbers_fn import numbers
-
-def happy_toad():
-    h = "toad!"
-    # print("{}: {}".format(h, str(type(h))))
-    h = ['t', 'o', 'a', 'd']
-    # print("{}: {}".format(h, type(h)))
-    print("I'm a happy {}".format(h))
-    # print(type("I'm a happy {}".format(h)))
-    return 0
+def make_world_go_round(world, speed, happy=True):
+    print("The {} is going round at speed {} knots".format(world, speed))
 
 
-def random_with_toad():
-    for i in range(32):
-        # print("i: {}".format(type(i)))
-        r = random.randrange(0, 1024)
-        # print("r: {}".format(type(r)))
-        print(r)
-        if r % 5 == 0:
-            # print("r % 5 == 0: {}".format(type(r % 5 == 0)))
-            happy_toad()
+# arguments unpacking
+def some_generic_fn(a, *args, c, **kwargs):
+    print("a: {}".format(a))
+    print("Args: {}".format(args))
+    print("Kwargs: {}".format(kwargs))
 
 
 def main():
-    # random_with_toad()
-    numbers()
+    # make_world_go_round("Earth", 120)
+    make_world_go_round(speed=120, happy=False, world="Earth")
+    make_world_go_round("Earth", speed=120)
+    # make_world_go_round(speed=120, "Earth")  # не работает,
+    # неименованные нельзя после хоть  1го  именованного
+    make_world_go_round(speed=120, world="Earth")  # работает
 
+    some_generic_fn(5, 3, 'a', s='s', t=[1, 2], f=(5+3), c='just a string')
+    # some_generic_fn(5, 3, 'a', s='s', [1, 2], f=(5+3))  # не работает
 
 main()
